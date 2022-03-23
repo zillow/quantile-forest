ee3dd8b2dc7beab9b2dc2707f3e24aa5c24477a4
reidjohnson <reidjohnson@users.noreply.github.com>
2022-03-23 21:44:15 +0000

    deploy: 27574fe0d0924d0475025e184af62c9646f9153f

---
+++ _downloads/c207f81bef888578d9542a91738a445b/plot_quantile_vs_standard_forest.py
@@ -0,0 +1,51 @@
+"""
+=========================================================
+Quantile regression forest vs. standard regression forest
+=========================================================
+
+An example comparison between a quantile regression forest and a standard
+regression forest on a synthetic, right-skewed dataset. In a right-skewed
+distribution, the mean is to the right of the median.
+
+"""
+print(__doc__)
+
+import matplotlib.pyplot as plt
+import scipy as sp
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.utils.validation import check_random_state
+
+from quantile_forest import RandomForestQuantileRegressor
+
+
+rng = check_random_state(0)
+
+# Create right-skewed dataset.
+n_samples = 5000
+a, loc, scale = 5, -1, 1
+skewnorm_rv = sp.stats.skewnorm(a, loc, scale)
+skewnorm_rv.random_state = rng
+y = skewnorm_rv.rvs(n_samples)
+X = rng.randn(n_samples, 2) * y.reshape(-1, 1)
+
+regr_rf = RandomForestRegressor(n_estimators=10, random_state=0)
+regr_qrf = RandomForestQuantileRegressor(n_estimators=10, random_state=0)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.5, random_state=0
+)
+
+regr_rf.fit(X_train, y_train)
+regr_qrf.fit(X_train, y_train)
+
+y_pred_rf = regr_rf.predict(X_test)
+y_pred_qrf = regr_qrf.predict(X_test, quantiles=0.5)
+
+colors = ["#c0c0c0", "#e69f00", "#56b4e9"]
+names = ["Actual", "RF (Mean)", "QRF (Median)"]
+plt.hist([y_test, y_pred_rf, y_pred_qrf], bins=50, color=colors, label=names)
+plt.xlabel("Actual and Predicted Target Values")
+plt.ylabel("Counts")
+plt.legend()
+plt.show()
---
+++ _sources/api.rst.txt
@@ -0,0 +1,17 @@
+#############
+API Reference
+#############
+
+This is the full API documentation of the `quantile-forest` package.
+
+.. currentmodule:: quantile_forest
+
+Quantile Forests
+================
+
+.. autosummary::
+   :toctree: generated/
+   :template: class.rst
+
+   ExtraTreesQuantileRegressor
+   RandomForestQuantileRegressor
