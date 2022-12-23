8ad6495f35254520a0686b4635e0ab3822907000
Reid Johnson <reid.johnson@gmail.com>
2022-12-23 04:56:00 -0800

    Update setup metadata

--- setup.py
+++ setup.py
@@ -41,10 +41,10 @@ CLASSIFIERS = [
     "Operating System :: POSIX",
     "Operating System :: Unix",
     "Operating System :: MacOS",
-    "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
     "Programming Language :: Python :: Implementation :: CPython",
 ]
 
