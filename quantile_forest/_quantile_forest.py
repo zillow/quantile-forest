0bdc756b839c231d4051db0e24218b87a6fff06c
Reid Johnson <reidj@zillowgroup.com>
2024-09-06 03:31:29 -0700

    Update docstrings

--- quantile_forest/_quantile_forest.py
+++ quantile_forest/_quantile_forest.py
@@ -686,7 +686,8 @@ class BaseForestQuantileRegressor(ForestRegressor):
 
         Returns
         -------
-        y_pred : array of shape (n_samples, n_outputs, n_quantiles)
+        y_pred : array of shape (n_train, n_quantiles) or \
+                (n_train, n_outputs, n_quantiles)
             If quantiles is set to 'mean', then return ``E(Y | X)``. Else, for
             all quantiles, return ``y`` at ``q`` for which ``F(Y=y|x) = q``,
             where ``q`` is the quantile.
